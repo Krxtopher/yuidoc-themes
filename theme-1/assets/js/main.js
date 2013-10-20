cfe2454d95f251ef6996b137a17df4ffb7510dda
Kris Schultz <kristopher@writeme.com>
2013-10-19 23:14:08 -0400

    Solution (incomplete) for revealing jump links using jQuery Waypoints.

--- theme-1/assets/js/main.js
+++ theme-1/assets/js/main.js
@@ -4,61 +4,22 @@ $(document).ready(function() {
 
 	"use strict";
 
-	$(window).scroll(function() {
-		
-		for (var i = scrollDetectors.length - 1; i >= 0; i--) {
-			scrollDetectors[i]();
+	// Set up link reveal for index anchor link.
+	$(".index").waypoint(function (direction) {
+		if (direction == "down") {
+			$("body").addClass("index-offscreen");
+		} else {
+			$("body").removeClass("index-offscreen");
 		}
-
 	});
 
-	var scrollDetectors = [];
-
-	function addOffscreenTopDetector(triggerItemSelector, styleTargetSelector, styleName) {
-
-		var triggerItem$ = $(triggerItemSelector);
-		var triggerItemBottom = triggerItem$.offset().top + triggerItem$.height();
-
-		var detector = function() {
-
-			var windowScrollPosition = $(window).scrollTop();
-
-			if (windowScrollPosition > triggerItemBottom) {
-				$(styleTargetSelector).addClass(styleName);
-			} else {
-				$(styleTargetSelector).removeClass(styleName);
-			}
-
-		};
-
-		scrollDetectors.push(detector);
-
-	}
-
-	function addLeavingScreenTopDetector(triggerItemSelector, styleTargetSelector, styleName) {
-
-		var triggerItem$ = $(triggerItemSelector);
-		var triggerItemTop = triggerItem$.offset().top;
-
-		var detector = function() {
-
-			var windowScrollPosition = $(window).scrollTop();
-			
-			if (windowScrollPosition > triggerItemTop) {
-				$(styleTargetSelector).addClass(styleName);
-			} else {
-				$(styleTargetSelector).removeClass(styleName);
-			}
-
-		};
-
-		scrollDetectors.push(detector);
-
-	}
-
-	// Setup
-
-	addOffscreenTopDetector(".index-section", "body", "offscreen-top-index-section");
-	addLeavingScreenTopDetector(".header", ".header", "sticky");
+	// Setup stickiness of page header.
+	$(".header").waypoint(function (direction) {
+		if (direction =="down") {
+			$(".header").addClass("sticky");
+		} else {
+			$(".header").removeClass("sticky");
+		}
+	}, {offset:-1});
 
 });
